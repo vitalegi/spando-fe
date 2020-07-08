c80afa810b6cce95980d79df7f09cb4042cd5c96
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2020-07-08 22:50:38 +0200

    added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,40 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/spando">Spando</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import Vue from "vue";
 
-#nav {
-  padding: 30px;
+export default Vue.extend({
+  name: "App",
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  components: {},
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+  data: () => ({
+    //
+  })
+});
+</script>
---
+++ src/views/Spando.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>This is an spando page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "spando-main",
+  data() {
+    return {
+      pending: 10
+    };
+  },
+  computed: {
+    dialog(): boolean {
+      return this.pending > 0;
+    }
+  }
+});
+</script>
+
+<style scoped></style>
